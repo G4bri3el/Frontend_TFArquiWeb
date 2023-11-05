90214d2c6ee0e80f5f78e7c1034f18502c77de1c
María Fernanda Medina <medina.mafer27@gmail.com>
2023-11-05 16:44:10 -0500

    ruta

--- FrontendArquiWeb/src/app/app-routing.module.ts
+++ FrontendArquiWeb/src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { CreaeditaUsuarioComponent } from './components/usuario/creaedita-usuario/creaedita-usuario.component';
+import { BicicletaComponent } from './components/bicicleta/bicicleta.component';
+import { CreaeditaBicicletaComponent } from './components/bicicleta/creaedita-bicicleta/creaedita-bicicleta.component';
 
 const routes: Routes = [
   {
     path: 'usuario', component: UsuarioComponent, children:[
       {path:'nuevo',component:CreaeditaUsuarioComponent},
+    ]
+  },
+  {
+    path: 'bicicleta', component:BicicletaComponent, children: [
+      {path: 'nuevo', component: CreaeditaBicicletaComponent}
     ]
   }
 ];
