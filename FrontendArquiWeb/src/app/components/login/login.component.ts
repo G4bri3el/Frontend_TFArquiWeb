0b58bf7bf12b4c9ac87aed93f5bf7e419e0382c6
G4br13l28 <u20211b661@upc.edu.pe>
2023-11-07 11:13:57 -0500

    feat: add login

--- FrontendArquiWeb/src/app/components/login/login.component.ts
+++ FrontendArquiWeb/src/app/components/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/model/usuario';
+import { UsuarioService } from 'src/app/service/usuario/usuario.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  usuario: Usuario = new Usuario();
+  mensaje: string = '';
 
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private uS: UsuarioService
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      usuarioCorreo: ['',Validators.required],
+      usuarioContrasena: ['',Validators.required],
+    });
+  }
+  aceptar(): void {
+
+
+
+    this.router.navigate(['usuario']);
+  }
 }
