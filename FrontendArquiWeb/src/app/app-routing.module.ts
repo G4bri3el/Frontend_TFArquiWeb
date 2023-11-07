890501217115fb6f4f329f29f3f4c128dd618c42
Yhadira Paccini <u20201f343@upc.edu.pe>
2023-11-06 19:21:50 -0500

    Routing de Reserva

--- FrontendArquiWeb/src/app/app-routing.module.ts
+++ FrontendArquiWeb/src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { CreaeditaUsuarioComponent } from './components/usuario/creaedita-usuario/creaedita-usuario.component';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { CreaeditaReservaComponent } from './components/reserva/creaedita-reserva/creaedita-reserva.component';
 
 const routes: Routes = [
   {
     path: 'usuario', component: UsuarioComponent, children:[
       {path:'nuevo',component:CreaeditaUsuarioComponent},
       {path:'empresaio', component:CreaeditaUsuarioComponent},
+    ]
+  },
+  {
+    path:'reserva', component:ReservaComponent, children: [
+      {path:'nuevo', component:CreaeditaReservaComponent}
     ]
   }
 ];
