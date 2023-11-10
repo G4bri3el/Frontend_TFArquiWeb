7f8bad00fc0394e10f5e72b623135422ca5b07f6
iShinmazinger <mazinger05x72@gmail.com>
2023-11-10 17:17:08 -0500

    ruta añadida

--- FrontendArquiWeb/src/app/service/image.service.ts
+++ FrontendArquiWeb/src/app/service/image.service.ts
@@ -15,6 +15,6 @@ export class ImageService {
 
     formData.append('image',image);
 
-    return this.http.post<Response>('/',formData);
+    return this.http.post<Response>('/media/upload',formData);
   }
 }
