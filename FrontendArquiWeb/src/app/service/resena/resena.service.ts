7d675f9a6096ede72edbad37b6a961588dadedd4
Yhadira Paccini <u20201f343@upc.edu.pe>
2023-11-13 21:23:51 -0500

    servicio de resena

--- FrontendArquiWeb/src/app/service/resena/resena.service.ts
+++ FrontendArquiWeb/src/app/service/resena/resena.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
-
+import { Resena } from 'src/app/model/resena';
+import { Subject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const base_url = environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class ResenaService {
+  private url = `${base_url}/resenas`;
 
-  constructor() { }
+  private listaCambio = new Subject<Resena[]>();
+  constructor(private http:HttpClient) {}
+
+  list() {
+    let token = sessionStorage.getItem('token');
+
+    return this.http.get<Resena[]>(this.url, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
+  }
+
+  
+  insert(re: Resena) {
+    let token = sessionStorage.getItem('token');
+
+    return this.http.post(this.url, re, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
+  }
+  setList(listaNueva: Resena[]) {
+    this.listaCambio.next(listaNueva);
+  }
+  getList() {
+    return this.listaCambio.asObservable();
+  }
+
 }
